3c3faeb18d7e9b4cca5e5be4f73a2d7c1f9e3e5b
Mehmet Basbug <mbasbug@mbasbug-pc.(none)>
2013-03-17 09:34:34 -0400

    Version tag is added.

--- pboost/__init__.py
+++ pboost/__init__.py
@@ -1,6 +1,7 @@
 __version_info__ = {
     'major': 0,
-    'minor': 1
+    'minor': 1,
+    'build': 1,
 }
 
 
@@ -8,7 +9,7 @@ def get_version():
     """
     Return the formatted version information
     """
-    vers = "%(major)i.%(minor)i" % __version_info__
+    vers = "%(major)i.%(minor)i.%(build)i" % __version_info__
     return vers
 
 __version__ = get_version()
